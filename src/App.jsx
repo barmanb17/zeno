ff8b9b45a15c7d463b8982ad80362a218daa211f
Bijoy Barman <bijoy20-127@aec.ac.in>
2025-07-11 21:53:23 +0530

    feat: add /ai layout, nested routes, and related assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import BlogTitles from './pages/BlogTitles'
+import WriteArticle from './pages/WriteArticle'
+import Layout from './pages/Layout'
+import RemoveBackground from './pages/RemoveBackground'
+import GenerateImages from './pages/GenerateImages'
+import RemoveObject from './pages/RemoveObject'
+import ReviewResume from './pages/ReviewResume'
+import Community from './pages/Community'
+import NotFound from './pages/NotFound'
+
+
 
 const App = () => {
   return (
     <div>
-      <h1>Welcome to the React App</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/ai' element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path='write-article' element={<WriteArticle />} />
+          <Route path='blog-titles' element={<BlogTitles />} />
+          <Route path='generate-images' element={<GenerateImages />} />
+          <Route path='remove-background' element={<RemoveBackground />} />
+          <Route path='remove-object' element={<RemoveObject />} />
+          <Route path='review-resume' element={<ReviewResume />} />
+          <Route path ='community' element={<Community />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
     </div>
   )
 }
