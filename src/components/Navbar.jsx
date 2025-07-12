70b20aa3f3d853c822e4c6f321fe28cce2a5e69d
Bijoy Barman <bijoy20-127@aec.ac.in>
2025-07-12 18:19:19 +0530

    feat: auth + navbar complete

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
+import { ArrowRight } from 'lucide-react'
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
+
+const Navbar = () => {
+    const navigate = useNavigate()
+    const { user } = useUser()
+    const { openSignIn } = useClerk()
+    return (
+        <div className='fixed top-0 left-0 w-full z-50 bg-white/80 backdrop-blur-xl border-b border-gray-200/20 flex justify-center py-2'>
+            <div className='max-w-7xl w-full flex justify-between items-center px-4 sm:px-8 xl:px-16'>
+                <div className='flex items-center gap-2 sm:gap-3 cursor-pointer' onClick={() => navigate('/')}>
+                    <img
+                        src={assets.logo}
+                        alt="logo"
+                        className='w-14 sm:w-12 cursor-pointer'
+                        onClick={() => navigate('/')}
+                    />
+                    <span className='text-4xl font-medium'>zeno.ai</span>
+                </div>
+
+                {
+                    user ? <UserButton /> : (<button
+                        className='flex items-center gap-2 rounded-full text-xs sm:text-sm cursor-pointer bg-primary text-white px-6 py-2 hover:bg-primary/90 transition-colors'
+                        onClick={openSignIn}
+                    >
+                        Get started <ArrowRight className='w-3 h-3 sm:w-4 sm:h-4' />
+                    </button>
+                    )
+                }
+
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
